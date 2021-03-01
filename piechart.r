bc591a2d6853a5ad80e46efedd70ccc0505303c5
Tom Hubbard <i@trh.me>
2021-03-01 15:49:44 -0500

    Create piechart.r

---
+++ piechart.r
@@ -0,0 +1,7 @@
+cat("Thanks to the demo(graphics) that comes with R.\n")
+cat("They say that pie charts are inferior to bar charts, because people are better at distinguishing differences in length than in area.")
+pie.sales <- c(0.12, 0.3, 0.26, 0.16, 0.04, 0.12)
+names(pie.sales) <- c("Blueberry", "Cherry", "Apple", "Boston Cream", "Other", "Vanilla Cream")
+pie(pie.sales, col = c("purple","violetred1","green3","cornsilk","cyan","white"))
+title(main = "January Pie Sales", cex.main = 1.8, font.main = 1)
+title(xlab = "(Don't try this at home kids)", cex.lab = 0.8, font.lab = 3)
